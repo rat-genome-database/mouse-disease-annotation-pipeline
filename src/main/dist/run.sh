c07e2a7884c0dcf1b731e4c12321d5dc4c08130f
Marek Tutaj <mtutaj@mcw.edu>
2018-12-20 07:59:40 -0600

    updated dependencies

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -14,10 +14,8 @@ fi
 
 
 cd $APPDIR
-pwd
-DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db.xml"
-LOG4J_OPTS="-Dlog4j.configuration=file://$APPDIR/properties/log4j.properties"
-declare -x "MOUSE_DISEASE_OPTS=$DB_OPTS $LOG4J_OPTS"
-bin/$APPNAME "$@" 2>&1 | tee run.log
+java -Dspring.config=$APPDIR/../properties/default_db.xml \
+    -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
+    -jar lib/${APPNAME}.lib "$@" 2>&1 | tee run.log
 
 mailx -s "[$SERVER] Mouse DO Annotation pipeline OK" $EMAIL_LIST < $APPDIR/logs/status.log
