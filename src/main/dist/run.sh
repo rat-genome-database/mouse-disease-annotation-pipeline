49805ddf318fddfb2c3401eab9f32e03d2fc7788
tutajm <mtutaj@mcw.edu>
2022-09-08 16:47:44 -0500

    fixed summary email

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -18,4 +18,4 @@ java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -jar lib/${APPNAME}.jar "$@" 2>&1 | tee run.log
 
-mailx -s "[$SERVER] Mouse DO Annotation pipeline OK" $EMAIL_LIST < $APPDIR/logs/status.log
+mailx -s "[$SERVER] Mouse DO Annotation pipeline OK" $EMAIL_LIST < $APPDIR/logs/summary.log
